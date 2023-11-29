1e230bc7eb4a5a0b021f8f257c7fc0e4eca28e31
Ohleesang <jammmmed@gmail.com>
2023-11-29 17:25:40 +0900

    문제 이해하는데 한 몇시간은 소비한거같다.. 만약 이해가 안된다면 반복문이 어떻게 도는지 직접 생각해보고 해보자

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -14,20 +14,24 @@ class Solution {
         var answer = 0
         citations.sortDescending()
         var hIndex = 1
+
         for(i in 1.. citations.size){
-            if(i>=citations[i-1]) hIndex = maxOf(citations[i-1],i)
             //hIndex 는 논문수를 넘어가선 안된다
+            if(i<=citations[i-1]) hIndex = i
         }
+
         /**
-         * i = 1 (1,[6])    hIndex = 1
-         * i = 2 (2,[6,5])  hIndex = 2
-         * i = 3 (3,[6,5,3]) hIndex = 3
-         * i = 4 (4,[6,5,3,1]) hIndex = 3
-         * i = 5 (5,[6,5,3,1,0]) hIndex = 3
+         * i = 1 (1,[10])    hIndex = 1
+         * i = 2 (2,[10,8])  hIndex = 2
+         * i = 3 (3,[10,8,5]) hIndex = 3
+         * i = 4 (4,[10,8,5,4]) hIndex = 4
+         * i = 5 (5,[10,8,5,4,3]) hIndex = 4
          *
          * i 랑 cit[i-1]를 비교
-         * > 만약. i>=cit[i-1]
+         * > 만약. i>=cit[i-1] hIndex = i
+         * i<=cit[i-1]
          */
+        answer = hIndex
         return answer
     }
 }
@@ -35,8 +39,8 @@ class Solution {
 fun main(){
 
     var a = Solution()
-    a.solution(intArrayOf(3,0,6,1,5))
-    a.solution(intArrayOf(0,0,1))
+//    a.solution(intArrayOf(3,0,6,1,5))
+//    a.solution(intArrayOf(0,0,1))
     a.solution(intArrayOf(10,8,5,4,3))
     a.solution(intArrayOf(25,8,5,3,3))
 }
